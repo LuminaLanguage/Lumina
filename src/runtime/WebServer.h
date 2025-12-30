1e25fcc415d84da63d7e0f5abed6475f74d282bc
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 14:11:07 +0000

    Update WebServer.h

--- src/runtime/WebServer.h
+++ src/runtime/WebServer.h
@@ -1,17 +1,33 @@
 #pragma once
 #include <string>
 #include <functional>
-#include <map>
-#include "Request.h"
-#include "Response.h"
+#include <vector>
 #include <crow.h>
 
-class WebServer {
+class Request {
 public:
-    int port;
-    crow::SimpleApp app;
+    std::string path;
+    std::string method;
+    std::map<std::string, std::string> query;
+    std::string body;
+};
 
-    void listen(int p);
-    void on(const std::string& method, const std::string& path,
-            std::function<void(Request&, Response&)> callback);
+class Response {
+public:
+    int status = 200;
+    std::string body;
+    void send(const std::string& data) { body = data; }
+    void json(const std::string& data) { body = data; } // can parse stringified JSON later
+};
+
+class WebServer {
+private:
+    crow::SimpleApp app;
+    std::vector<std::function<void(Request&, Response&)>> middlewares;
+
+public:
+    void listen(int port);
+    void use(std::function<void(Request&, Response&)> fn);
+    void get(const std::string& path, std::function<void(Request&, Response&)> fn);
+    void post(const std::string& path, std::function<void(Request&, Response&)> fn);
 };
