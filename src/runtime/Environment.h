87c4ac5faafc491b7bce4dcf9d72994d27b0e1c7
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 13:43:37 +0000

    Create Environment.h

---
+++ src/runtime/Environment.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <map>
+#include <string>
+#include "Variable.h"
+
+class Environment {
+public:
+    std::map<std::string, Variable> vars;
+    void set(const std::string& name, const Variable& val);
+    Variable get(const std::string& name);
+};
