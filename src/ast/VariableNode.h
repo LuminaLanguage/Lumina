346a39b25f5ccb325648e0356ffc897378ad99fe
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 13:48:37 +0000

    Create VariableNode.h

---
+++ src/ast/VariableNode.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Node.h"
+#include <string>
+
+class VariableNode : public Node {
+public:
+    std::string name;
+    VariableNode(const std::string& n) : Node("variable"), name(n) {}
+};
