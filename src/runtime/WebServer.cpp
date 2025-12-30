cc795971a845b413b457236d7207e9c84b36de8d
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 14:14:01 +0000

    Update WebServer.cpp

--- src/runtime/WebServer.cpp
+++ src/runtime/WebServer.cpp
@@ -1,38 +1,55 @@
 #include "WebServer.h"
 
-void WebServer::listen(int p) {
-    port = p;
+void WebServer::listen(int port) {
     app.port(port).multithreaded().run();
 }
 
-void WebServer::on(const std::string& method, const std::string& path,
-                   std::function<void(Request&, Response&)> callback) {
-    if(method == "GET") {
-        CROW_ROUTE(app, path) ([callback](const crow::request& req_crow) {
-            Request req;
-            Response res;
+void WebServer::use(std::function<void(Request&, Response&)> fn) {
+    middlewares.push_back(fn);
+}
 
-            req.path = req_crow.url;
-            req.method = "GET";
-            req.body = req_crow.body;
+void WebServer::handleRoute(const crow::request& req_crow, std::function<void(Request&, Response&)> fn, const std::string& method) {
+    Request req;
+    Response res;
+    req.method = method;
+    req.path = req_crow.url;
+    req.body = req_crow.body;
 
-            callback(req, res);
+    for(auto& mw : middlewares) mw(req, res);
+    fn(req, res);
+}
 
-            return res.body;
-        });
-    } else if(method == "POST") {
-        CROW_ROUTE(app, path).methods(crow::HTTPMethod::Post)
-        ([callback](const crow::request& req_crow) {
-            Request req;
-            Response res;
+// GET
+void WebServer::get(const std::string& path, std::function<void(Request&, Response&)> fn) {
+    CROW_ROUTE(app, path)([this, fn](const crow::request& req_crow){ handleRoute(req_crow, fn, "GET"); return ""; });
+}
 
-            req.path = req_crow.url;
-            req.method = "POST";
-            req.body = req_crow.body;
+// POST
+void WebServer::post(const std::string& path, std::function<void(Request&, Response&)> fn) {
+    CROW_ROUTE(app, path).methods(crow::HTTPMethod::Post)([this, fn](const crow::request& req_crow){ handleRoute(req_crow, fn, "POST"); return ""; });
+}
 
-            callback(req, res);
+// PUT
+void WebServer::put(const std::string& path, std::function<void(Request&, Response&)> fn) {
+    CROW_ROUTE(app, path).methods(crow::HTTPMethod::Put)([this, fn](const crow::request& req_crow){ handleRoute(req_crow, fn, "PUT"); return ""; });
+}
 
-            return res.body;
-        });
-    }
+// PATCH
+void WebServer::patch(const std::string& path, std::function<void(Request&, Response&)> fn) {
+    CROW_ROUTE(app, path).methods(crow::HTTPMethod::Patch)([this, fn](const crow::request& req_crow){ handleRoute(req_crow, fn, "PATCH"); return ""; });
+}
+
+// OPTIONS
+void WebServer::options(const std::string& path, std::function<void(Request&, Response&)> fn) {
+    CROW_ROUTE(app, path).methods(crow::HTTPMethod::Options)([this, fn](const crow::request& req_crow){ handleRoute(req_crow, fn, "OPTIONS"); return ""; });
+}
+
+// DELETE
+void WebServer::del(const std::string& path, std::function<void(Request&, Response&)> fn) {
+    CROW_ROUTE(app, path).methods(crow::HTTPMethod::Delete)([this, fn](const crow::request& req_crow){ handleRoute(req_crow, fn, "DELETE"); return ""; });
+}
+
+// HEAD
+void WebServer::head(const std::string& path, std::function<void(Request&, Response&)> fn) {
+    CROW_ROUTE(app, path).methods(crow::HTTPMethod::Head)([this, fn](const crow::request& req_crow){ handleRoute(req_crow, fn, "HEAD"); return ""; });
 }
