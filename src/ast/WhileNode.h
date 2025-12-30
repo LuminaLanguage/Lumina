8d51a571c7cd3dc8326c9eea1ecde16c1bc52ee1
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 13:59:36 +0000

    Create WhileNode.h

---
+++ src/ast/WhileNode.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Node.h"
+#include <vector>
+#include "../ast/Statement.h"
+#include <string>
+
+class WhileNode : public Node {
+public:
+    std::string condition;
+    std::vector<Statement*> body;
+    WhileNode(const std::string& cond) : Node("while"), condition(cond) {}
+};
