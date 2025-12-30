fb7f46e64ab812e1813eb68a62e41ce84631f966
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 13:55:56 +0000

    Create FunctionExecuter.cpp

---
+++ src/interpreter/FunctionExecuter.cpp
@@ -0,0 +1,8 @@
+#include "FunctionExecutor.h"
+#include "../interpreter/Executor.h"
+
+void FunctionExecutor::execute(FunctionNode* node, Environment& env) {
+    env.set(node->name, Variable()); // placeholder for function object
+    Executor executor(env);
+    executor.execute(node->body);
+}
