cd3360e4a0f2ffd27c6046d11cee542659941b53
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 13:42:29 +0000

    Create Parser.cpp

---
+++ src/interpreter/Parser.cpp
@@ -0,0 +1,13 @@
+#include "Parser.h"
+#include "Tokenizer.h"
+
+std::vector<Statement*> Parser::parse(const std::string& code) {
+    Tokenizer tokenizer;
+    auto tokens = tokenizer.tokenize(code);
+    std::vector<Statement*> statements;
+    for(auto &line : tokens) {
+        Statement* stmt = new Statement(line);
+        statements.push_back(stmt);
+    }
+    return statements;
+}
