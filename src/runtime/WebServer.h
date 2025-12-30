07ff64d3a617034e922a3fed1666c689677e971f
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 14:14:27 +0000

    Update WebServer.h

--- src/runtime/WebServer.h
+++ src/runtime/WebServer.h
@@ -17,7 +17,7 @@ public:
     int status = 200;
     std::string body;
     void send(const std::string& data) { body = data; }
-    void json(const std::string& data) { body = data; } // can parse stringified JSON later
+    void json(const std::string& data) { body = data; }
 };
 
 class WebServer {
@@ -25,9 +25,16 @@ private:
     crow::SimpleApp app;
     std::vector<std::function<void(Request&, Response&)>> middlewares;
 
+    void handleRoute(const crow::request& req_crow, std::function<void(Request&, Response&)> fn, const std::string& method);
+
 public:
     void listen(int port);
     void use(std::function<void(Request&, Response&)> fn);
     void get(const std::string& path, std::function<void(Request&, Response&)> fn);
     void post(const std::string& path, std::function<void(Request&, Response&)> fn);
+    void put(const std::string& path, std::function<void(Request&, Response&)> fn);
+    void patch(const std::string& path, std::function<void(Request&, Response&)> fn);
+    void options(const std::string& path, std::function<void(Request&, Response&)> fn);
+    void del(const std::string& path, std::function<void(Request&, Response&)> fn);
+    void head(const std::string& path, std::function<void(Request&, Response&)> fn);
 };
