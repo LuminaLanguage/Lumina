46088283abc2c36f39c08ae9b9f5fa966b98891e
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 14:06:01 +0000

    Create MathUtils.cpp

---
+++ src/utils/MathUtils.cpp
@@ -0,0 +1,6 @@
+#include "MathUtils.h"
+
+double MathUtils::add(double a, double b) { return a + b; }
+double MathUtils::sub(double a, double b) { return a - b; }
+double MathUtils::mul(double a, double b) { return a * b; }
+double MathUtils::div(double a, double b) { return b != 0 ? a / b : 0; }
