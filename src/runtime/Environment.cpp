5a3471f9a1d7d6d1a79f72ccb23af6dea4ddf0af
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 13:43:53 +0000

    Create Environment.cpp

---
+++ src/runtime/Environment.cpp
@@ -0,0 +1,10 @@
+#include "Environment.h"
+
+void Environment::set(const std::string& name, const Variable& val) {
+    vars[name] = val;
+}
+
+Variable Environment::get(const std::string& name) {
+    if(vars.count(name)) return vars[name];
+    return Variable();
+}
