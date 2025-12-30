8040dc1dcf4529878488c66000b9fc0a9730ddf6
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 13:41:30 +0000

    Create Interpreter.h

---
+++ src/interpreter/Interpreter.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+#include "Parser.h"
+#include "Executor.h"
+#include "../runtime/Environment.h"
+
+class Interpreter {
+public:
+    Environment global;
+    void run(const std::string& code);
+};
