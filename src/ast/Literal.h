a05fb5de613947eb2a9a8c5c062791108c64bf5c
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 13:46:37 +0000

    Create Literal.h

---
+++ src/ast/Literal.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Expression.h"
+#include <string>
+
+class Literal : public Expression {
+public:
+    std::string strValue;
+    double numValue;
+    bool isString = true;
+    Literal(const std::string& val) : Expression({val}), strValue(val), numValue(0), isString(true) {}
+    Literal(double val) : Expression({}), strValue(""), numValue(val), isString(false) {}
+};
