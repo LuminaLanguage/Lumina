c950f2cf7399a04b3d03dd71c64b20f070b8074d
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 14:07:25 +0000

    Create StdIO.cpp

---
+++ src/stdlib/StdIO.cpp
@@ -0,0 +1,11 @@
+#include "StdIO.h"
+#include <iostream>
+
+void StdIO::print(const std::string& msg) { std::cout << msg << std::endl; }
+
+std::string StdIO::input(const std::string& prompt) {
+    std::cout << prompt;
+    std::string val;
+    std::getline(std::cin, val);
+    return val;
+}
