d01c07df52eba9e72b43aaa1e3add48bf9868274
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 13:47:35 +0000

    Create Executer.h

---
+++ src/interpreter/Executer.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "../ast/Statement.h"
+#include "../runtime/Environment.h"
+#include <vector>
+
+class Executor {
+    Environment& env;
+public:
+    Executor(Environment& e) : env(e) {}
+    void execute(const std::vector<Statement*>& stmts);
+    void executeStatement(Statement* stmt);
+};
