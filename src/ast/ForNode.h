1f1734478de0edbbae670ab0b2b538e8c7930d9e
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 13:50:54 +0000

    Create ForNode.h

---
+++ src/ast/ForNode.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Node.h"
+#include <vector>
+#include "../ast/Statement.h"
+#include <string>
+
+class ForNode : public Node {
+public:
+    std::string varName;
+    std::string start;
+    std::string end;
+    std::vector<Statement*> body;
+    ForNode(const std::string& v, const std::string& s, const std::string& e)
+        : Node("for"), varName(v), start(s), end(e) {}
+};
