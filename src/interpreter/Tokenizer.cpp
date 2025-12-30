68081d4768521793b2e46cc2989a8f8c3afa06b7
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 13:43:09 +0000

    Create Tokenizer.cpp

---
+++ src/interpreter/Tokenizer.cpp
@@ -0,0 +1,16 @@
+#include "Tokenizer.h"
+#include <sstream>
+
+std::vector<std::vector<std::string>> Tokenizer::tokenize(const std::string& code) {
+    std::vector<std::vector<std::string>> result;
+    std::istringstream iss(code);
+    std::string line;
+    while(std::getline(iss, line)) {
+        std::istringstream lss(line);
+        std::vector<std::string> tokens;
+        std::string word;
+        while(lss >> word) tokens.push_back(word);
+        result.push_back(tokens);
+    }
+    return result;
+}
