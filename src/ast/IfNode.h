9d7c74a258a4b688ea7e0fcf1db2fe19403947f5
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 13:49:54 +0000

    Create IfNode.h

---
+++ src/ast/IfNode.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Node.h"
+#include <vector>
+#include "../ast/Statement.h"
+
+class IfNode : public Node {
+public:
+    std::string condition;
+    std::vector<Statement*> thenBranch;
+    std::vector<Statement*> elseBranch;
+    IfNode(const std::string& cond) : Node("if"), condition(cond) {}
+};
