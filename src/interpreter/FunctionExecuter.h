5ed8a3e4bbdca03f848ee25ee37ead09cd099f19
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 13:54:59 +0000

    Create FunctionExecuter.h

---
+++ src/interpreter/FunctionExecuter.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "../ast/FunctionNode.h"
+#include "../runtime/Environment.h"
+
+class FunctionExecutor {
+public:
+    static void execute(FunctionNode* node, Environment& env);
+};
