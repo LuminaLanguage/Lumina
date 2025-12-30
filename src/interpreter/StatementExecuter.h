d030e7415d8b74b01bc27ed62d182f86f58da28b
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 13:53:19 +0000

    Create StatementExecuter.h

---
+++ src/interpreter/StatementExecuter.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "../ast/Statement.h"
+#include "../runtime/Environment.h"
+
+class StatementExecutor {
+public:
+    static void execute(Statement* stmt, Environment& env);
+};
