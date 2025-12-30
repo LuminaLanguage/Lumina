bf764ba7d699c5d97a8d03841c82e50b8addfc20
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 13:54:28 +0000

    Create ControlFlow.cpp

---
+++ src/interpreter/ControlFlow.cpp
@@ -0,0 +1,27 @@
+#include "ControlFlow.h"
+#include "../interpreter/Executor.h"
+#include "../interpreter/ExpressionEvaluator.h"
+
+void ControlFlow::executeIf(IfNode* node, Environment& env) {
+    Variable cond = ExpressionEvaluator::evaluate(node->condition, env);
+    Executor executor(env);
+    if(cond.num != 0) executor.execute(node->thenBranch);
+    else executor.execute(node->elseBranch);
+}
+
+void ControlFlow::executeFor(ForNode* node, Environment& env) {
+    double start = std::stod(node->start);
+    double end = std::stod(node->end);
+    Executor executor(env);
+    for(double i=start;i<end;i++) {
+        env.set(node->varName, Variable(i));
+        executor.execute(node->body);
+    }
+}
+
+void ControlFlow::executeWhile(WhileNode* node, Environment& env) {
+    Executor executor(env);
+    while(ExpressionEvaluator::evaluate(node->condition, env).num != 0) {
+        executor.execute(node->body);
+    }
+}
