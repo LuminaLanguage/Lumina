a0b49ba5b164d34359fba358407bad08e0ca3422
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-12-30 14:10:50 +0000

    Update WebServer.cpp

--- src/runtime/WebServer.cpp
+++ src/runtime/WebServer.cpp
@@ -1,11 +1,38 @@
 #include "WebServer.h"
-#include <iostream>
 
 void WebServer::listen(int p) {
     port = p;
-    std::cout << "Server listening on port " << port << std::endl;
+    app.port(port).multithreaded().run();
 }
 
-void WebServer::on(const std::string& method, const std::string& path, std::function<void(Request&, Response&)> callback) {
-    routes[method + ":" + path] = callback;
+void WebServer::on(const std::string& method, const std::string& path,
+                   std::function<void(Request&, Response&)> callback) {
+    if(method == "GET") {
+        CROW_ROUTE(app, path) ([callback](const crow::request& req_crow) {
+            Request req;
+            Response res;
+
+            req.path = req_crow.url;
+            req.method = "GET";
+            req.body = req_crow.body;
+
+            callback(req, res);
+
+            return res.body;
+        });
+    } else if(method == "POST") {
+        CROW_ROUTE(app, path).methods(crow::HTTPMethod::Post)
+        ([callback](const crow::request& req_crow) {
+            Request req;
+            Response res;
+
+            req.path = req_crow.url;
+            req.method = "POST";
+            req.body = req_crow.body;
+
+            callback(req, res);
+
+            return res.body;
+        });
+    }
 }
